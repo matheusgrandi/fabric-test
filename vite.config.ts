e1b19458a93125d064772650594ee6bf34111ca4
matheusgrandi <matheus@matheusgrandi.com.br>
2025-09-20 11:29:44 -0300

    extensions setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "public/manifest.json",
+          dest: ".",
+        },
+      ],
+    }),
+  ],
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      input: {
+        main: "./index.html",
+      },
+    },
+  },
+});
